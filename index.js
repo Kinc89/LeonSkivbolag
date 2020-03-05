48f84f862b4ce8e52c15712a0737437fc772b85f
bbkfed <balthazarberling+fed@gmail.com>
2020-03-05 15:53:24 +0100

    major changes in file structure on the backend

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const mongoose = require('mongoose');
 
-const { app, port } = require('./src/routes/server');
+const express = require('express');
+const sassMiddleware = require('node-sass-middleware');
+const app = express();
+
 const dbConfig = require('./config/config');
+const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(sassMiddleware({ // tell sassMiddleware where src file and dest directory is
+    src: 'sass',
+    dest: 'public',
+    // debug: true, // för att skriva ut data till konsollen
+    outputStyle: 'compressed'
+}));
+// define a static folder, 'public'
+app.use(express.static('public'));
+// express body-parser
+app.use(express.urlencoded({ extended: true }));
+// view engine to use, ejs in this case
+app.set('view engine', 'ejs');
+
+// routes variables
+const mainRoute = require("./src/routes/mainRoute");
+
+// routes use
+app.use(mainRoute);
 
 // Start servern
 const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true };
@@ -9,4 +33,4 @@ mongoose.connect(dbConfig.databaseURL, dbOptions).then(() => {
     app.listen(port, () => console.log(`App listening on port ${port}! http://localhost:4000`))
 });
 
-module.exports = { app, port };
+// module.exports = { app, port };
