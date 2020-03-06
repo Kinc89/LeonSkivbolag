39598d4a224f9a906d4865ccced779f37b25398b
yaman-du <yamandu@gmail.com>
2020-03-06 14:44:51 +0100

    added album.ejs and working foreach on main

--- src/functions/getLastFmData.js
+++ src/functions/getLastFmData.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch');
 
-async function getLastFmData () {
+async function getLastFmData() {
 
     const requestOptions = {
         method: 'GET',
         redirect: 'follow'
     };
 
-    return await fetch("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=a9bd98edbf62437e2542a87294e156da&artist=Cher&album=Believe&format=json", requestOptions)
+    return await fetch("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=a9bd98edbf62437e2542a87294e156da&artist=kanye&album=graduation&format=json", requestOptions)
         .then(res => res.json())
         .catch(error => console.log('error', error));
 }
