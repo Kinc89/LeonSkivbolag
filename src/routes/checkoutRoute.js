31a13ecd215604ad68cc422e111b44106ba35783
bbkfed <balthazarberling+fed@gmail.com>
2020-03-16 15:44:18 +0100

    work on permissions, almost done, most pages work

--- src/routes/checkoutRoute.js
+++ src/routes/checkoutRoute.js
@@ -5,20 +5,29 @@ const { ROUTE, VIEW } = require("./variables");
 const User = require("../../model/user");
 
 const verifyToken = require("../middlewares/verifyToken");
+const checkUser = require("../middlewares/checkUser");
 
-app.get(ROUTE.checkout, verifyToken, async (req, res) => {
+app.get(ROUTE.checkout, verifyToken, checkUser, async (req, res) => {
 
 // who's the user, what contains its cart?
 
-
-// create an order number and save it to database to the specific user
-
 // where are coming the data from here?
 // const user = await User.findById({})
+
+console.log("get on checkout route");
 const user = "here will be USER data";
 
 res.render(VIEW.checkout, { user });
 
+});
+
+app.post(ROUTE.checkout, verifyToken, checkUser, async (req, res) => {
+
+    // take in all the data from the form in checkout page and create an order number and save it to database to the specific user
+    console.log("post on checkout route");
+    
+    res.redirect(VIEW.confirmation);
+    
 });
 
 module.exports = app;
--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -11,9 +11,13 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const verifyToken = require("../middlewares/verifyToken");
 
-app.get(ROUTE.login, (req, res) => {
+app.get(ROUTE.login, verifyToken, (req, res) => {
 
-    res.render(VIEW.login, { foundUser: true, invalidUser: false });
+    if (!req.validCookie || req.validCookie.user.status === "guest") return res.render(VIEW.login, { foundUser: true, invalidUser: false });
+
+    if (req.validCookie.user.status === "user") return res.redirect(ROUTE.userProfile);
+
+    if (req.validCookie.user.status == "admin") return res.redirect(ROUTE.admin);
 
 });
 
@@ -22,7 +26,7 @@ app.post(ROUTE.login, async (req, res) => {
     const user = await User.findOne({ username: req.body.username });
     console.log("USER IS ->", user);
 
-    if (!user) return res.render(VIEW.login, { foundUser: false });
+    if (!user) return res.render(VIEW.login, { foundUser: false, invalidUser: false });
 
     // compare with database info
     const validUser = await bcrypt.compare(req.body.password, user.password); // true?
@@ -34,9 +38,7 @@ app.post(ROUTE.login, async (req, res) => {
         if (err) return res.redirect("/login");
 
         if (token) {
-            console.log(token);
-            // cookie-parser must be installed and required in index.js for that to work
-            
+
             if (!req.cookie) {
                 res.cookie("jsonwebtoken", token, { maxAge: 3600000, httpOnly: true })
             };
