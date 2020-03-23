a83f089fbf16b1176bd104c11da818e6521c869e
juliafumiko <juliafumikodahl@gmail.com>
2020-03-23 13:21:19 +0100

    small changes

--- src/routes/checkoutRoute.js
+++ src/routes/checkoutRoute.js
@@ -6,19 +6,16 @@ const verifyToken = require("../middlewares/verifyToken");
 const checkUser = require("../middlewares/checkUser");
 
 app.get(ROUTE.checkout, verifyToken, checkUser, async (req, res) => {
-
-    
-    const user = req.validCookie.user;
     
 
 // who's the user, what contains its cart?
 const  order = req.validCookie.user.cart
-const placeOrder = await User.findById({ _id: req.validCookie.user._id }); //"here will be USER data";
-order.push(placeOrder);
+// const placeOrder =  //"here will be USER data";
+// order.push(placeOrder);
 
 
 // where are coming the data from here?
-// const user = await User.findById({})
+const user = await User.findById({ _id: req.validCookie.user._id });
 
 console.log("get on checkout route");
       
